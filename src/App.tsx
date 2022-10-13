86ea76194cb841f12a00e2ecfc12f4d55cbb203d
Jan Cibulka <78412925+jannden@users.noreply.github.com>
2022-10-13 18:11:12 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React 18 template</h1>
+      <h2>With TypeScript and React Router Dom</h2>
+    </div>
+  );
+}
